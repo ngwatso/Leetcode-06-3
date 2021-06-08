2ddde262ce347caa1618d42feae3ce219ca62ba4
Nick Watson <>
2021-06-08 14:53:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        
+        res = []
+        
+        if root != None:
+            self.inorderTraversalHelper(root, res)      
+        return res
+    
+    def inorderTraversalHelper(self, root, res):
+        
+        if root == None:
+            return
+        # go to left subtree first
+        if root.left != None:
+            self.inorderTraversalHelper(root.left, res)
+        # process current node
+        res.append(root.val)
+        # got to right subtree next
+        if root.right != None:
+            self.inorderTraversalHelper(root.right, res)
+        
+'''
+
+U:
+
+[1, null, 2, 3]
+
+output = [1, 3, 2]
+
+[1, 2, null, null, 3, 4, 5]
+
+output = [2, 1, 4, 3, 5]
+
+P:
+
+Use recursion to go to the left subtree first, then process the current node, then use recursion again to go to the right subtree next
+
+'''
+
+# ===============
